bd9937f05f12d9606a1cf41c628f8b5b2e9c1c90
Sakumzi <skenjanakeith@gmail.com>
2022-09-08 16:25:11 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 /**
+ *
  * main - print the string in the put function
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main(void)
+int main(void) 
 {
 	char c;
 	int i;
@@ -17,7 +18,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof((c));
 	printf("Size of an int: %ld byte(s)\n", sizeof((i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof((li));
+	printf("Size of a long int: %ld byte(s)\n", sizeof((li);
 	printf("Size of a long long int: %ld byte(s)\n", sizeof((lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof((f));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof((lli));
+	return (0);
 }
